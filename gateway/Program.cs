5f39fde711776f0dcf0c58c9d564354f81e270f1
grn-dev <mahmoudsabzalii@gmail.com>
2021-07-22 01:49:06 +0430

    Feat : Config  Ocelot

--- gateway/Program.cs
+++ gateway/Program.cs
@@ -1,11 +1,9 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace gateway
 {
@@ -13,14 +11,19 @@ namespace gateway
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args);
+
+            builder.ConfigureServices(s => s.AddSingleton(builder))
+                    .ConfigureAppConfiguration(
+                          ic => ic.AddJsonFile(Path.Combine("ocelot.json")))
+                    .UseStartup<Startup>();
+            var host = builder.Build();
+            return host;
+        }
+    } 
 }
--- gateway/Startup.cs
+++ gateway/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +30,9 @@ namespace gateway
         {
 
             services.AddControllers();
+
+            services.AddOcelot();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "gateway", Version = "v1" });
@@ -35,7 +40,7 @@ namespace gateway
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -54,6 +59,8 @@ namespace gateway
             {
                 endpoints.MapControllers();
             });
+            await app.UseOcelot();
+
         }
     }
 }
